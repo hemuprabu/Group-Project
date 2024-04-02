16ae997e43317400c226412479dbd3dc29ad0760
James Egues <jregues@me.com>
2024-04-02 14:06:37 -0400

    added headers obj for petfinders api authorization token- will edit and add real token later (James)

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// The following code is to create a object containing authorization key for the petfinder api, and will allow us to fetch data from the api. I (James) will generate a token for us to use
+// recieved the following code from ai generated answer when asked how to access api data with an authorization token through javascript
+// const myHeaders = new Headers();
+// myHeaders.append('Content-Type', 'application/json');
+// myHeaders.append('Authorization', 'your-token'); // Replace 'your-token' with your actual token
